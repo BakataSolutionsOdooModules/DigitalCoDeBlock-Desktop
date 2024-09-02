579cd2ec33f4621dab0430cc01993671eea5e366
javierduquebakata <duque.informatica@bakata.es>
2024-09-02 14:55:41 +0100

    First Commit

---
+++ scripts/lib/libraries.js
@@ -0,0 +1,13 @@
+const backdrops = require('digitalcodeblock-gui/src/lib/libraries/backdrops.json');
+const costumes = require('digitalcodeblock-gui/src/lib/libraries/costumes.json');
+const sounds = require('digitalcodeblock-gui/src/lib/libraries/sounds.json');
+const sprites = require('digitalcodeblock-gui/src/lib/libraries/sprites.json');
+
+const libraries = {
+    backdrops,
+    costumes,
+    sounds,
+    sprites
+};
+
+module.exports = libraries;
